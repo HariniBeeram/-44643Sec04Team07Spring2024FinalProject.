122bcf050d6ff20918eed2edef989dcf4a6633d0
Harini Beeram <s565877@nwmissouri.edu>
2024-02-23 17:19:34 -0600

    added title and tagline in lottie view

--- SHA-STORY/SHA-STORY/SHA_STORY.swift
+++ SHA-STORY/SHA-STORY/SHA_STORY.swift
@@ -10,18 +10,38 @@ import Lottie
 
 class SHA_STORY: UIViewController {
     
-    
-    @IBOutlet weak var LaunchLAV: LottieAnimationView!{
+    @IBOutlet weak var titleAnimationLBL: UILabel!{
         didSet{
-            LaunchLAV.animation = .named("Animation")
+            titleAnimationLBL.text = "SHA Story"
+            titleAnimationLBL.font = UIFont.boldSystemFont(ofSize: 18)
+            titleAnimationLBL.font = UIFont.boldSystemFont(ofSize: 12)
+                                        
+        }
+    }
+    @IBOutlet weak var lottileView: UIView!{
+        didSet{
+            LaunchLAV.animation = .named("cheflogo")
             LaunchLAV.alpha=1
             LaunchLAV.play(){[weak self] _ in
                 UIViewPropertyAnimator.runningPropertyAnimator(withDuration:1, delay: 0.1, options: [.curveEaseIn]){
-                    self!.LaunchLAV.alpha = 0
+                    self!.lottileView.alpha = 0
+                    
+                    
                 }
             }
         }
     }
+    
+    @IBOutlet weak var taglineLBL: UILabel!{
+        didSet{
+            taglineLBL.text = "Where every style tells a story"
+        }
+    }
+    
+    
+    
+    
+    @IBOutlet weak var LaunchLAV: LottieAnimationView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
