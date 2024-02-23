abaa5a56b0ccbb7b3934d2f312d45ef7a47c6259
Amogh Nalla <S566278@nwmissouri.edu>
2024-02-23 01:03:03 -0600

    Updated lottie

--- SHA-STORY/SHA-STORY/SHA_STORY.swift
+++ SHA-STORY/SHA-STORY/SHA_STORY.swift
@@ -10,23 +10,23 @@ import Lottie
 
 class SHA_STORY: UIViewController {
     
+    
+    @IBOutlet weak var LaunchLAV: LottieAnimationView!{
+        didSet{
+            LaunchLAV.animation = .named("Animation")
+            LaunchLAV.alpha=1
+            LaunchLAV.play(){[weak self] _ in
+                UIViewPropertyAnimator.runningPropertyAnimator(withDuration:1, delay: 0.1, options: [.curveEaseIn]){
+                    self!.LaunchLAV.alpha = 0
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //Testing branch
         // Do any additional setup after loading the view.
     }
     
-    
-    @IBOutlet weak var LaunchLAV : LottieAnimationView!{
-        didSet {
-            LaunchLAV.animation = LottieAnimation.named("cheflogo")
-            LaunchLAV.alpha = 1
-            LaunchLAV.play { [weak self] _ in
-                UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1.0, delay: 0.0, options: [.curveEaseInOut]) {
-                    self?.LaunchLAV.alpha = 0.0
-                }
-            }
-        }
-        
-    }
 }
