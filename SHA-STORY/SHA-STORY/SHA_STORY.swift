bd487dc016016e4b3376ff22b433bde95b303506
Harini Beeram <s565877@nwmissouri.edu>
2024-02-22 22:21:29 -0600

    added lottie

--- SHA-STORY/SHA-STORY/SHA_STORY.swift
+++ SHA-STORY/SHA-STORY/SHA_STORY.swift
@@ -6,15 +6,27 @@
 //
 
 import UIKit
+import Lottie
 
 class SHA_STORY: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Testing branch 
+        //Testing branch
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBOutlet weak var LaunchLAV : LottieAnimationView!{
+        didSet {
+            LaunchLAV.animation = LottieAnimation.named("cheflogo")
+            LaunchLAV.alpha = 1
+            LaunchLAV.play { [weak self] _ in
+                UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1.0, delay: 0.0, options: [.curveEaseInOut]) {
+                    self?.LaunchLAV.alpha = 0.0
+                }
+            }
+        }
+        
+    }
 }
-
