dbecdb2ced04a2dac80f48afdbda3db55aec028d
Amogh Nalla <S566278@nwmissouri.edu>
2024-03-08 19:53:24 -0600

    added connections and some functionality to the registration page

--- SHA-STORY/RegistrationViewController.swift
+++ SHA-STORY/RegistrationViewController.swift
@@ -8,14 +8,40 @@
 import UIKit
 
 class RegistrationViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var UsernameTF: UITextField!
+    
+    
+    @IBOutlet weak var PasswordTF: UITextField!
+    
+    
+    @IBOutlet weak var ConfirmPasswordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        PasswordTF.isEnabled=false
+        ConfirmPasswordTF.isEnabled=false
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func userName(_ sender: UITextField) {
+        
+        guard let text=UsernameTF.text, !text.isEmpty else{return}
+        PasswordTF.isEnabled=true
+    }
+    
+    
+    @IBAction func password(_ sender: UITextField) {
+        
+        guard let passText=PasswordTF.text, !passText.isEmpty else{return}
+        ConfirmPasswordTF.isEnabled=true
+    }
+    
+    @IBAction func SignupBTN(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
