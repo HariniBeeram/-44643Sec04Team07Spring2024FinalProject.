0adc99f42ce402637e47384e5e20689554a2efb2
Amogh Nalla <S566278@nwmissouri.edu>
2024-04-12 21:24:59 -0500

    added connections to the Delivery VC

---
+++ SHA-STORY/SHA-STORY/DeliveryViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DeliveryViewController.swift
+//  SHA-STORY
+//
+//  Created by ios 1 on 12/04/24.
+//
+
+import UIKit
+
+class DeliveryViewController: UIViewController {
+    
+    
+    @IBOutlet weak var Picker: UILabel!
+    
+    
+    
+    @IBOutlet weak var DPicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func ContinueBTN(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
